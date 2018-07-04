039ca5a4bf41ccc7f8218f10241c199c049f57fb
dosdarwin <s103395@cmes.tn.edu.tw>
2018-07-04 21:49:24 +0800

    second

--- product.py
+++ product.py
@@ -5,7 +5,6 @@ while True:
 		break
 	p = []
 	price = input('please type the price of the product you buy:') 
-	p.append(name)
-	p.append(price)
-	product.append(p)
-print(product)
+	product.append([name, price])
+for p in product:
+	print(p[0], 'cost is', p[1])
