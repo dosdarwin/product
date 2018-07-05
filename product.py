e2a2d0a439e80a16fd3629ea7ba2d59ac4c445c5
dosdarwin <s103395@cmes.tn.edu.tw>
2018-07-05 21:20:14 +0800

    open with excel

--- product.py
+++ product.py
@@ -8,3 +8,7 @@ while True:
 	product.append([name, price])
 for p in product:
 	print(p[0], 'cost is', p[1])
+with open('product.csv', 'w') as f:
+	f.write('商品,價格\n')
+	for p in product:
+		f.write(p[0] + ',' + p[1] + '\n')
