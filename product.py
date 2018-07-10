69b860135bce5df9914ebe14eab175d958f1e271
dosdarwin <s103395@cmes.tn.edu.tw>
2018-07-10 20:43:27 +0800

    final2

--- product.py
+++ product.py
@@ -1,4 +1,4 @@
-product =[]
+product = []
 while True:
 	name = input('please type the product you buy:')
 	if name == 'q':
@@ -12,12 +12,16 @@ with open('product.csv', 'w') as f:
 	f.write('商品,價格\n')
 	for p in product: 
 		f.write(p[0] + ',' + p[1] + '\n')
-
+import os
 pro2=[]
-with open('product.csv', 'r') as r:
-	for d in r:
-		if '商品,價格' in d:
-			continue
-		name,price = d.strip().split(',')
-		pro2.append([name,price])
+if os.path.isfile('product.csv'):
+	print('yeah!, find the file!')
+	with open('product.csv', 'r') as r:
+		for d in r:
+			if '商品,價格' in d:
+				continue
+			name,price = d.strip().split(',')
+			pro2.append([name,price])
+else:
+	print('cannot find the file......')
 print(pro2)
