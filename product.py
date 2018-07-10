2094e8c1fc93c63bfe181430b22b8e3eea08391d
dosdarwin <s103395@cmes.tn.edu.tw>
2018-07-10 20:19:55 +0800

    final

--- product.py
+++ product.py
@@ -10,5 +10,14 @@ for p in product:
 	print(p[0], 'cost is', p[1])
 with open('product.csv', 'w') as f:
 	f.write('商品,價格\n')
-	for p in product:
+	for p in product: 
 		f.write(p[0] + ',' + p[1] + '\n')
+
+pro2=[]
+with open('product.csv', 'r') as r:
+	for d in r:
+		if '商品,價格' in d:
+			continue
+		name,price = d.strip().split(',')
+		pro2.append([name,price])
+print(pro2)
